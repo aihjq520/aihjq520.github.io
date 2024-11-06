4266e841b372bc4a0b0afa361813fe2e063eea9e
more <morehe@canway.net>
2024-11-06 18:32:12 +0800

    Site updated: 2024-11-06 18:32:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/11/large-file-download/","2024/01/11/handwrite-promise/","2023/05/25/webpack-1/","2024/10/16/vue3-1/","2024/01/16/type-and-interface/","2024/11/02/网络安全/"];function toRandomPost(){
+var posts=["2024/01/11/large-file-download/","2024/01/11/handwrite-promise/","2023/05/25/webpack-1/","2024/10/16/vue3-1/","2024/01/16/type-and-interface/","2024/11/02/网络安全/","2021/07/15/浏览器缓存/","2021/06/15/nginx设置缓存/","2023/06/15/worker/","2023/06/15/work-experience/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
