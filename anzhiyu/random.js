fc9b4016ddc35011f585ffbd797c2108269068cb
more <morehe@canway.net>
2024-11-12 17:37:10 +0800

    Site updated: 2024-11-12 17:36:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/11/large-file-download/","2024/01/11/handwrite-promise/","2023/05/25/webpack-1/","2024/10/16/vue3-1/","2024/01/16/type-and-interface/","2024/11/02/网络安全/","2021/07/15/浏览器缓存/","2021/06/15/nginx设置缓存/","2023/06/15/worker/","2023/06/15/work-experience/","2022/07/02/react总结1/","2022/07/02/react总结2/","2024/04/19/work-experience2/","2022/07/03/react总结3/"];function toRandomPost(){
+var posts=["2024/01/11/large-file-download/","2024/01/11/handwrite-promise/","2023/05/25/webpack-1/","2024/10/16/vue3-1/","2024/01/16/type-and-interface/","2024/11/02/网络安全/","2021/07/15/浏览器缓存/","2021/06/15/nginx设置缓存/","2023/06/15/worker/","2023/06/15/work-experience/","2022/07/02/react总结1/","2022/07/02/react总结2/","2024/04/19/work-experience2/","2022/07/03/react总结3/","2024/11/12/doccker学习2/","2022/07/27/gitlabcicd/","2023/10/20/微前端学习记录/","2022/07/27/docker学习1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
